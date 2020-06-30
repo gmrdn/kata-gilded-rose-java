cfa9b861ce0dfdc80e6d872b0e485c9bf16417e8
Guillaume R <guillaume.rondon@gmail.com>
2020-06-30 10:03:38 +0200

    Writing some approval tests

--- gildedrose-kata/Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ gildedrose-kata/Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
+    void nameStaysTheSameWhenUpdatingQuality() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+    }
+
+
+    @Test
+    void qualityOf0Stays0whenSellinPassed() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void qualityOf0Stays0whileSellin() {
+        Item[] items = new Item[] { new Item("foo", 0, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void qualityOf50Becomes48whenSellinPassed() {
+        Item[] items = new Item[] { new Item("foo", 0, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(48, app.items[0].quality);
+    }
+
+    @Test
+    void qualityOf50Becomes49whileInSellin() {
+        Item[] items = new Item[] { new Item("foo", 1, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(49, app.items[0].quality);
+    }
+
+    @Test
+    void sellinLowersEachDay() {
+        Item[] items = new Item[] { new Item("foo", 10, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, app.items[0].sellIn);
     }
 
 }
